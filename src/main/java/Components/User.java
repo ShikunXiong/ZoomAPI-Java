4231096b1ef7920b569509ee34aca892de6d1332
dlwangyilin <dlwangyilin@gmail.com>
2020-04-16 23:59:06 -0700

    Update two sections

--- src/main/java/Components/User.java
+++ src/main/java/Components/User.java
@@ -2,6 +2,11 @@ package Components;
 
 import Utils.HttpUtils;
 import okhttp3.MediaType;
+import okhttp3.RequestBody;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 public class User {
     String token;
@@ -12,5 +17,54 @@ public class User {
         this.util = new HttpUtils();
     }
 
+    public String listAllUsers(String... strs) throws IOException {
+        String role_id = strs[0];
+        String url = "/users" + "?role_id=" + role_id;
+        String data = this.util.getRequest(url, this.token);
+        JSONObject jsonObject = new JSONObject(data);
+        JSONArray jsonArray = jsonObject.getJSONArray("users");
+        return jsonArray.toString();
+    }
 
+    public String createUsers(String... strs) throws IOException {
+        String url = "/users";
+        JSONObject json = new JSONObject();
+        json.put("action", strs[0]);
+        JSONObject user_info = new JSONObject();
+        user_info.put("email", strs[1]);
+        user_info.put("type", Integer.parseInt(strs[2]));
+        json.put("user_info", user_info);
+        RequestBody body = RequestBody.create(JSON, json.toString());
+        return this.util.postRequest(url, this.token, body);
+    }
+
+    public String updateUser(String... strs) throws IOException {
+        String url = "/users/%1$s" + "?login_type=%2$s";
+        url = String.format(url, strs[0], strs[1]);
+        JSONObject json = new JSONObject();
+        json.put("first_name", strs[2]);
+        json.put("last_name", strs[3]);
+        json.put("type", Integer.parseInt(strs[4]));
+        json.put("pmi", Integer.parseInt(strs[5]));
+        json.put("phone_number", strs[6]);
+        RequestBody body = RequestBody.create(JSON, json.toString());
+        return this.util.postRequest(url, this.token, body);
+    }
+
+    public void deleteUser(String... strs) throws IOException {
+        String url = "/users/me"
+                   + "?action=%1$s"
+                   + "&transfer_email=%2$s"
+                   + "&transfer_meeting=%3$s"
+                   + "&transfer_webinar=%4$s"
+                   + "&transfer_recording=%5$s";
+        url = String.format(url, strs);
+        this.util.deleteRequest(url, this.token);
+    }
+
+    public String getUser(String... strs) throws IOException {
+        String url = "/user/me" + "?login_type=%s";
+        url = String.format(url, strs[0]);
+        return this.util.getRequest(url, this.token);
+    }
 }
--- src/main/java/Components/Webinar.java
+++ src/main/java/Components/Webinar.java
@@ -1,4 +1,84 @@
 package Components;
 
+import Utils.HttpUtils;
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
 public class Webinar {
+    String token;
+    HttpUtils util;
+    private final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    public Webinar(String token){
+        this.token = token;
+        this.util = new HttpUtils();
+    }
+
+    public String listWebinars(String... strs) throws IOException {
+        String url = "/users/me/webinars";
+        String data = this.util.getRequest(url, this.token);
+        JSONObject json = new JSONObject(data);
+        JSONArray jsonArray = json.getJSONArray("webinars");
+        return jsonArray.toString();
+    }
+
+    public void createWebinar(String... strs) throws IOException {
+        String url = "/users/me/webinars";
+        JSONObject json = new JSONObject();
+        json.put("topic", strs[0]);
+        json.put("type", Integer.parseInt(strs[1]));
+        json.put("start_time", strs[2]);
+        json.put("duration", Integer.parseInt(strs[3]));
+        json.put("timezone", strs[4]);
+        json.put("password", strs[5]);
+        json.put("agenda", strs[6]);
+        RequestBody body = RequestBody.create(JSON, json.toString());
+        this.util.postRequest(url, this.token, body);
+    }
+
+    public void updateWebinar(String... strs) throws IOException {
+        String url = "/webinars/%1$s" + "?occurrence_id=%2$s";
+        url = String.format(url, strs[0], strs[1]);
+        JSONObject json = new JSONObject();
+        json.put("topic", strs[0]);
+        json.put("type", Integer.parseInt(strs[1]));
+        json.put("start_time", strs[2]);
+        RequestBody body = RequestBody.create(JSON, json.toString());
+        this.util.patchRequest(url, this.token, body);
+    }
+
+    public void deleteWebinar(String... strs) throws IOException {
+        String url = "/webinars/%1$s" + "?occurrence_id=%2$s";
+        url = String.format(url, strs[0], strs[1]);
+        this.util.deleteRequest(url, this.token);
+    }
+
+    public void endWebinar(String... strs) throws IOException {
+        String url = "/webinars/%s/status";
+        url = String.format(url, strs[0]);
+        JSONObject json = new JSONObject();
+        json.put("action", "end");
+        RequestBody body = RequestBody.create(JSON, json.toString());
+        this.util.putRequest(url, this.token, body);
+    }
+
+    public String getWebinars(String... strs) throws IOException {
+        String url = "/webinars/%1$s" + "?occurrence_id=%2$s";
+        url = String.format(url, strs[0], strs[1]);
+        return this.util.getRequest(url, this.token);
+    }
+
+    public void addWebinarRegistrant(String... strs) throws IOException {
+        String url = "/webinars/%1$s/registrants" + "?occurrence_id=%2$s";
+        url = String.format(url, strs[0], strs[1]);
+        JSONObject json = new JSONObject();
+        json.put("email", strs[0]);
+        json.put("first_name", strs[1]);
+        json.put("last_name", strs[2]);
+        RequestBody body = RequestBody.create(JSON, json.toString());
+        this.util.postRequest(url, this.token, body);
+    }
 }
